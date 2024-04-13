eefc7fe872b45691ce08ddeb3de989579f61fbdd
Xey <Xeybien@icloud.com>
2024-04-13 00:40:57 -0400

    Two changes

--- Assets/Scripts/RotateYScript.cs
+++ Assets/Scripts/RotateYScript.cs
@@ -11,6 +11,6 @@ public class RotateYScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(0.0f, 0.5f, 0.0f);
-	}
+		transform.Rotate(0.0f, 0.2f, 0.0f);
+	
 }
